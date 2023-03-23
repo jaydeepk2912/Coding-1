387a5fb866823b0f73b427cddbf9731aba3584ad
jaydeepk2912 <jaydeepk2912@gmail.com>
2023-03-23 17:20:31 -0400

    updated

--- customerservice/src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
+++ customerservice/src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
@@ -1,14 +1,18 @@
 package edu.iu.p565.customerservice.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import edu.iu.p565.customerservice.model.Customer;
 
 @RestController
-@RequestMapping
-public class HomeController {
+@RequestMapping("/customers")
+public class CustomerController {
+    
     @GetMapping
-    public String greetings(){
-        return "Greetings!";
+    public List<Customer> findAll(){
+        return null;
     }
 }
